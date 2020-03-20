799310ae32659ad59e0ffb86f1b2667db4a95187
luke <Lukas H>
2020-03-20 15:24:11 +0100

    master

--- js/rachkova-navigation.js
+++ js/rachkova-navigation.js
@@ -259,4 +259,38 @@ function enableScroll() {
     document.onkeydown = null;
 }
 
-// viewport check
+// Burger
+(function($) {
+   $('.item-transition').on('click', function() {
+      $(this).toggleClass('active');
+   });
+
+   $('.item-animation').on('click', function() {
+      if($(this).hasClass('active-in')) {
+         $(this).removeClass('active-in');
+         $(this).addClass('active-out');
+      } else {
+         $(this).removeClass('active-out');
+         $(this).addClass('active-in');
+      }
+   });
+})(jQuery);
+
+
+const rippleElements = document.getElementsByClassName("myRipple");
+
+for(let i = 0; i < rippleElements.length; i++) {
+  rippleElements[i].onclick = function(e){
+    let X = e.pageX - this.offsetLeft;
+    let Y = e.pageY - this.offsetTop;
+    let rippleDiv = document.createElement("div");
+    rippleDiv.classList.add('ripple');
+    rippleDiv.setAttribute("style","top:"+Y+"px; left:"+X+"px;");
+    let customColor = this.getAttribute('ripple-color');
+    if (customColor) rippleDiv.style.background = customColor;
+    this.appendChild(rippleDiv);
+    setTimeout(function(){
+      rippleDiv.parentElement.removeChild(rippleDiv);
+    }, 900);
+  }
+}
